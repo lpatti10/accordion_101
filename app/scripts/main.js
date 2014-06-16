e9c4c1d5c32cc738486b88a7b91f2ad825ac4483
Laura Patti <laurapatti@Lauras-MacBook-Pro.local>
2014-06-16 18:02:58 -0400

    First jQuery function working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,29 @@
-$('a.title').on('click', function() {
-	$('p.content').css('.collapse');
+$('a.titlebar').on('click', function() {
+	$('p.contentHide').removeClass('contentHide').addClass('contentShow');
 });
+
+		$(this).toggleClass('contentHide');
+
+
+
+
+
+
+
+/*
+
+<script type="text/javascript">
+  $(document).ready(function($) {
+    $('#accordion').find('.accordion-toggle').click(function(){
+
+      //Expand or collapse this panel
+      $(this).next().slideToggle('fast');
+
+      //Hide the other panels
+      $(".accordion-content").not($(this).next()).slideUp('fast');
+
+    });
+  });
+</script>
+
+*/
