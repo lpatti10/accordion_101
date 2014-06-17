710fe612d68be432beeaa7d17124c62566f1dedb
Laura Patti <laurapatti@Lauras-MacBook-Pro.local>
2014-06-16 22:15:29 -0400

    Got conditional toggle to work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -29,15 +29,46 @@ $('a.titlebar').on('click', function() {
 $('a.titlebar').on('click', function() {
   $('p.contentHide').toggleClass('contentShow', 'contentHide');
 });
+
+
+// Does not work 
+$('a.titlebar').on('click', function() {
+  $(this).toggleClass('contentShow');
+});
+
+
+$('a.titlebar').on('click', function() {
+		$(this).parents().siblings('p.contentHide').addClass('contentShow');
+    $(this).parents('p.contentHide').removeClass('contentShow');
+});
+*/
+
+// SUCCESS, but havent figured out the conditional
+/*
+$('a.titlebar').on('click', function() {
+  $('p.contentHide').toggleClass('contentShow');
+});
 */
 
 $('a.titlebar').on('click', function() {
-  $('p.contentHide').toggleClass('contentShow', 'contentHide');
+		  $(this).next().toggleClass('contentShow');
+      $(".accordion-content").not($(this).next()).removeClass('contentShow');
 });
 
 
 
+
 /*
+$(document).ready(function() {
+  $("#accordion section h1").click(function(e) {
+    $(this).parents().siblings("section").addClass("ac_hidden");
+    $(this).parents("section").removeClass("ac_hidden");
+
+    e.preventDefault();
+  });
+});
+
+
 $( "p" ).click(function() {
   $( this ).toggleClass( "highlight" );
 });
